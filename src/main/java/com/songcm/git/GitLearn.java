e970a6e8d342aa8c61cc34b3ada6f2b07d247363
songcm <songcmend@163.com>
2021-09-27 16:57:28 +0800

    hot-fix first commit~

--- src/main/java/com/songcm/git/GitLearn.java
+++ src/main/java/com/songcm/git/GitLearn.java
@@ -4,5 +4,6 @@ public class GitLearn {
     public static void main(String[] args) {
         System.out.println("learn git");
         System.out.println("learn git go on");
+        System.out.println("hot-fix commit1");
     }
 }
