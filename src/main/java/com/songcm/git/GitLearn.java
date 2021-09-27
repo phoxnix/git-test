dceb3a651b70ce96fa34e86b624499422b3d7bb2
songcm <songcmend@163.com>
2021-09-27 16:47:03 +0800

    second commit~

--- src/main/java/com/songcm/git/GitLearn.java
+++ src/main/java/com/songcm/git/GitLearn.java
@@ -3,5 +3,6 @@ package com.songcm.git;
 public class GitLearn {
     public static void main(String[] args) {
         System.out.println("learn git");
+        System.out.println("learn git go on");
     }
 }
