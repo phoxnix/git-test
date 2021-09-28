3d54a77667944643467ea31aec1d7be7c969c377
songcm <songcmend@163.com>
2021-09-28 10:49:54 +0800

    hotfix commit ui

--- src/main/java/com/songcm/git/GitLearn.java
+++ src/main/java/com/songcm/git/GitLearn.java
@@ -6,5 +6,10 @@ public class GitLearn {
         System.out.println("learn git go on");
         System.out.println("hot-fix commit1");
         System.out.println("hot-fix commit2");
+
+
+
+
+        System.out.println("hot-fix ui");
     }
 }
